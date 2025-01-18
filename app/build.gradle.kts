651d19a4c513caee00a096af6d7d301eda21e7dc
Fextur <galavishay.ga@gmail.com>
2025-01-18 05:15:52 +0200

    added firebase storage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,5 +61,5 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.auth)
-
+    implementation(libs.firebase.storage)
 }
